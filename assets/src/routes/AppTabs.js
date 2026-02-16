0762361dc9b45432797e19380f4238fd6ea2f9b9
Ab1ga1l-tech <150353834+Ab1ga1l-tech@users.noreply.github.com>
2026-02-16 09:27:20 -0300

    Implement bottom tab navigation with screens

---
+++ assets/src/routes/AppTabs.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
+
+import HomeStack from './HomeStack';
+import CarrinhoScreen from '../screens/CarrinhoScreen';
+import PerfilScreen from '../screens/PerfilScreen';
+
+const Tab = createBottomTabNavigator();
+
+export default function AppTabs() {
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        headerShown: false,
+        tabBarIcon: ({ color, size }) => {
+          let iconName;
+
+          if (route.name === 'Home') {
+            iconName = 'home';
+          } else if (route.name === 'Carrinho') {
+            iconName = 'cart';
+          } else if (route.name === 'Perfil') {
+            iconName = 'person';
+          }
+
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: '#007bff',
+        tabBarInactiveTintColor: 'gray',
+      })}
+    >
+      <Tab.Screen name="Home" component={HomeStack} />
+      <Tab.Screen name="Carrinho" component={CarrinhoScreen} />
+      <Tab.Screen name="Perfil" component={PerfilScreen} />
+    </Tab.Navigator>
+  );
+}
